73268b4f59a6dc3ecfaa2c8ac47eba498e6f9ac3
Nathan Page <nathanalexanderpage@gmail.com>
2020-08-14 18:47:13 -0700

    included ID in class instance attributes

--- boardwatch_models/game.py
+++ boardwatch_models/game.py
@@ -1,5 +1,6 @@
 class Game():
-    def __init__(self, name, year_first_release, is_bootleg):
+    def __init__(self, id, name, year_first_release, is_bootleg):
+        self.id = id
         self.name = name
         self.year_first_release = year_first_release
         self.is_bootleg = is_bootleg
--- boardwatch_models/platform.py
+++ boardwatch_models/platform.py
@@ -1,5 +1,6 @@
 class Platform():
-    def __init__(self, name, is_brand_missing_from_name, platform_family_id, platform_family_name, model_no, storage_capacity, description, disambiguation, relevance, editions=[]):
+    def __init__(self, id, name, is_brand_missing_from_name, platform_family_id, platform_family_name, model_no, storage_capacity, description, disambiguation, relevance, editions=[]):
+        self.id = id
         self.name = name
         self.is_brand_missing_from_name = is_brand_missing_from_name
         self.platform_family_id = platform_family_id

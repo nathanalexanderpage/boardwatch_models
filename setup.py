03c98da5e95c713c033d76c83b02e672163f10e2
Nathan Page <nathanalexanderpage@gmail.com>
2020-10-08 17:22:07 -0700

    incremented to v0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="boardwatch-models",
-    version="0.4.0",
+    version="0.5.0",
     author="Nathan Alexander Page",
     author_email="nathanalexanderpage@gmail.com",
     description="Model classes for larger Boardwatch product",

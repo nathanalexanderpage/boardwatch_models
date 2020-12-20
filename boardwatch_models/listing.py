7a413c88b4dca8d07d8e405a907667f6e9275a54
Nathan Page <nathanalexanderpage@gmail.com>
2020-12-20 14:00:34 -0800

    ditched seller_phone, seller_email properties of Listing

--- boardwatch_models/listing.py
+++ boardwatch_models/listing.py
@@ -1,14 +1,13 @@
 class Listing():
 	registry = {}
 
-	def __init__(self, id, native_id, title, body, url, seller_email, seller_phone, date_posted, date_scraped):
+	def __init__(self, id, native_id, title, body, price, url, date_posted, date_scraped):
 		self.id = id
 		self.native_id = native_id
 		self.title = title
 		self.body = body
+		self.price = price
 		self.url = url
-		self.seller_email = seller_email
-		self.seller_phone = seller_phone
 		self.date_posted = date_posted
 		self.date_scraped = date_scraped
 

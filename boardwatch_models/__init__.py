50588f5bad7dc72099813de0264eb42f07cb2e42
Nathan Page <nathanalexanderpage@gmail.com>
2020-12-30 17:55:53 -0800

    User class imported into main module namespace

--- boardwatch_models/__init__.py
+++ boardwatch_models/__init__.py
@@ -4,3 +4,4 @@ from boardwatch_models.listing import Listing
 from boardwatch_models.platform import Platform
 from boardwatch_models.platform_edition import PlatformEdition
 from boardwatch_models.platform_name_group import PlatformNameGroup
+from boardwatch_models.user import User
